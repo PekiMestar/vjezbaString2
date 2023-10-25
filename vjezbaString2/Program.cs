21bab586f41c2123a31d121f2ede8182b21bcb01
PekiMestar <145533879+PekiMestar@users.noreply.github.com>
2023-10-25 08:17:23 +0200

    Add files via upload

---
+++ vjezbaString2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace vjezbaString2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string tekst;
+
+            Console.WriteLine("Napišite neki tekst.");
+            
+            tekst = Console.ReadLine();
+
+            Console.WriteLine(tekst.Replace(" ", "_"));
+
+            Console.ReadKey();
+        }
+    }
+}
